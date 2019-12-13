34942bf47d8eb6ba358cfe85cd8612f4b473b49e
Davi Holanda <daviht7@gmail.com>
2019-12-12 21:36:33 -0300

    primeiro commit

---
+++ src/main/java/com/mc/modelagem/ModelagemconceitualApplication.java
@@ -0,0 +1,13 @@
+package com.mc.modelagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelagemconceitualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelagemconceitualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mc/modelagem/ModelagemconceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mc.modelagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemconceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
