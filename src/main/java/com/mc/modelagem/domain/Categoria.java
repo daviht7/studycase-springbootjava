4e1b07531ff82f19cba5143965e4b29ebf34cca4
Davi Holanda <daviht7@gmail.com>
2019-12-14 14:26:33 -0300

    criando configuração de database

--- src/main/java/com/mc/modelagem/domain/Categoria.java
+++ src/main/java/com/mc/modelagem/domain/Categoria.java
@@ -2,10 +2,22 @@ package com.mc.modelagem.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
 	private String nome;
 	
 	public Categoria() {
