05410c93200115c2039b4e2c6ca129fd3b3a9906
Davi Holanda <daviht7@gmail.com>
2019-12-14 12:04:19 -0300

    criando classe de dominio Categoria

--- src/main/java/com/mc/modelagem/resources/CategoriaResource.java
+++ src/main/java/com/mc/modelagem/resources/CategoriaResource.java
@@ -1,16 +1,32 @@
 package com.mc.modelagem.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mc.modelagem.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "está ok";
+	public List<Categoria> listar() {
+		
+		Categoria c  = new Categoria();
+		c.setId(10);
+		c.setNome("davi");
+		Categoria c1  = new Categoria();
+		c1.setId(12);
+		c1.setNome("kelcya");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(c);
+		lista.add(c1);
+		return lista;
+		
 	}
 
 }
